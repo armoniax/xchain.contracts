78f5ad6f8a6849a3cbe3d965f87811f25e705a6a
masteray <masteraychen@gmail.com>
2022-06-11 20:41:12 +0800

    1st import

---
+++ xchain-ks-webapi/server/daemons/global.daemon.js
@@ -0,0 +1,31 @@
+/*
+  Global stat
+*/
+const { SETTINGS_DB, log, config, request } = require('./header')('global_stat');
+
+request.get(`${config.historyChain}/v1/history/get_actions`, (err, response, body) => {
+	if (err){
+		log.error(err);
+		process.exit(1);
+	}
+	
+	let data;
+	try{
+		log.info(`${config.historyChain}/v1/history/get_actions: ` + body);
+		data = JSON.parse(body);
+	} catch(err){
+		log.error(err);
+		process.exit(1);
+	}
+
+	SETTINGS_DB.findOneAndUpdate({}, {actions: data.actions, transactions: data.transactions}, (err) => {
+		if (err){
+			log.error(err);
+			process.exit(1);
+		}
+		log.info('Actions and Transactions successfully saved!');
+		process.exit();
+	});
+});
+
+
---
+++ xchain-ks-webapi/server/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name: `api`,
+    script    : `./server.js`
+    //instances : `2`,
+    //exec_mode : `cluster`
+  }],
+}
